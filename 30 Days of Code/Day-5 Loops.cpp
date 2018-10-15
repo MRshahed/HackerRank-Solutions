28aa02b1c81e4f976f87749cfbad0cd06de51565
Shahed hossen <shahedhossensaad@gmail.com>
2018-10-15 19:52:32 +0600

    Create Day-5 Loops.cpp

---
+++ 30 Days of Code/Day-5 Loops.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+
+int n;
+cin>>n;
+for(int i=1; i<=10; i++)
+{
+	cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+}
+}
