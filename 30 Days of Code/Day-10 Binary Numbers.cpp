5dcdf4975b3c10a4b97222176069ecc57e72173d
Shahed hossen <shahedhossensaad@gmail.com>
2018-11-04 13:19:42 +0600

    Update Day-10 Binary Numbers.cpp

--- 30 Days of Code/Day-10 Binary Numbers.cpp
+++ 30 Days of Code/Day-10 Binary Numbers.cpp
@@ -4,16 +4,31 @@ using namespace std;
 int main()
 {
     int n;
+    int reminder=0;
+    int count=0;
+    int max=0;
     cin>>n;
 
-    while(n>0)
-    {
-         int i=0;
-        int arr[100];
+        while(n!=0)
+        {
+        reminder=n%2;
+        n=n/2;
 
-        int reminder=n%2;
-                   n=n/2;
+        if(reminder==1)
+        {
+            count++;
+        }
+        else
+        {
+        count=0;
+        }   
+        if(count>max)
+        {
+        max=count;
+        }
+        }
+         cout<<max;
 
-    }
-    return 0;
+
+return 0;
 }
