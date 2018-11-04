23b36a9c4e24d098642721785d72ff912de711f8
Shahed hossen <shahedhossensaad@gmail.com>
2018-11-04 13:17:25 +0600

    Add files via upload

---
+++ 30 Days of Code/Day-10 Binary Numbers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    while(n>0)
+    {
+         int i=0;
+        int arr[100];
+
+        int reminder=n%2;
+                   n=n/2;
+
+    }
+    return 0;
+}
